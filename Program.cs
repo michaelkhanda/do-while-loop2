9787055a720add79fea20925e6a5c93235a645d4
Michael Khanda <michaelkhanda006@gmail.com>
2023-06-27 19:28:39 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DOWHILELOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score, total  = 0;
+            string answer = "N";
+            int i = 0;
+            int count = 1;
+
+            do
+            {
+                Console.WriteLine("Enter a score:");
+                score = int.Parse(Console.ReadLine());
+                total = total + score;
+                i++;
+                Console.WriteLine("Do you want to enter another score?(Y/N)");
+                answer = Console.ReadLine();
+                count = count + 1;
+
+            }
+            while (answer == "Y");
+            Double average = total/i;
+            Console.WriteLine("The average of the scores is: " + average);
+        }
+    }
+}
